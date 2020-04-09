d9f0636adb6a9c23e276d1f6ca0bb88a4e2f5b7e
zzyykk <zhouyk90@163.com>
2020-04-10 00:07:13 +0800

    cms_model内容添加

--- model/src/main/java/com/erudite/cms/model/CmsCategory.java
+++ model/src/main/java/com/erudite/cms/model/CmsCategory.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "cms_category")
 public class CmsCategory extends BaseEntity {
 
+    private String categoryName;
     private String ;
     private String ;
     private String ;
@@ -15,5 +16,6 @@ public class CmsCategory extends BaseEntity {
     private String ;
     private String ;
     private String ;
-    private String ;
+
+    
 }
